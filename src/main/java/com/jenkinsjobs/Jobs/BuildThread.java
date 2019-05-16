c9c7b6c281e6e8eb8f574fc67a04f29a5032dc1c
karthikkone <kirti.annajigar@ad.infosys.com>
2019-05-16 12:17:52 +0530

    Update BuildThread.java

--- src/main/java/com/jenkinsjobs/Jobs/BuildThread.java
+++ src/main/java/com/jenkinsjobs/Jobs/BuildThread.java
@@ -18,7 +18,8 @@ import net.sf.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-
+import org.springframework.stereotype.Component;
+@Component
 public class BuildThread implements Runnable {
 	
 		@Value("${jenkins.url}")
