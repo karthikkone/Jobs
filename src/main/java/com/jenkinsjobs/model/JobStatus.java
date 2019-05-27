731b094f7e5655a8f57f8c728f23e90c7fbb1d5c
karthikkone <kirti.annajigar@ad.infosys.com>
2019-05-27 10:07:34 +0530

    Update JobStatus.java

--- src/main/java/com/jenkinsjobs/model/JobStatus.java
+++ src/main/java/com/jenkinsjobs/model/JobStatus.java
@@ -1,19 +1,26 @@
 package com.jenkinsjobs.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
 
 import net.sf.json.JSONObject;
 
 @Entity
+
 public class JobStatus {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long buildid;	
 	private String buildname;
 	private String buildstatus;		
+	@Lob
+	@Column( length = 100000 )
+	private String logs;
 	
 	public JobStatus()
 	{
@@ -25,7 +32,13 @@ public class JobStatus {
 		this.buildname = buildname;
 		this.buildstatus = buildstatus;
 	}
-	
+
+	public String getLogs() {
+		return logs;
+	}
+	public void setLogs(String logs) {
+		this.logs = logs;
+	}
 	public Long getBuildid() {
 		return buildid;
 	}
